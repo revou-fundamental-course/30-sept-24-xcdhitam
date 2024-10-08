a1ced79850cdc2c1cdb5ea7a0ff42b004e23af5c
xcdhitam <xfebrian.cdhitam@gmail.com>
2024-10-09 03:31:03 +0700

    new post

--- js/reverse.js
+++ js/reverse.js
@@ -2,8 +2,10 @@
 * ==04-10-2024==
 * ini file js untuk tugas
 */
+/* ciptakan variabel param untuk mengambil data reverse di browser */
 let params = new URLSearchParams(document.location.search);
 let reverse = params.get("reverse");
+/* menyisipkan style css berdasarkan id yang dipilih pada variabel reverse */
 function hideSections(sections) {
     sections.forEach(anu => {
         const element = document.getElementById(anu);
@@ -15,7 +17,7 @@ function hideSections(sections) {
         }
     });
 }
-        
+/* kondisi dari nilai variabel reverse */   
 if (reverse === 'Fahrenheit') {
     hideSections([
         "anu-Celcius"
--- js/script.js
+++ js/script.js
@@ -2,3 +2,67 @@
 * ==04-10-2024==
 * ini file js untuk tugas
 */
+/* Ciptakan variabel dari elemen id css */
+const incelcius = document.getElementById('incelcius');
+const outfahrenheit = document.getElementById('outfahrenheit');
+const infahrenheit = document.getElementById('infahrenheit');
+const outcelcius = document.getElementById('outcelcius');
+/* Fungsi konversi dari hasil klik button */
+function konversicelcius() { 
+    hitungKonversicelcius(); /* Buat fungsi hitung konversi*/  
+    kalkulasicelcius(); /* Buat fungsi kalkulasi*/
+}
+function konversifahrenheit() { 
+    hitungKonversifahrenheit(); /* Buat fungsi hitung konversi*/ 
+    kalkulasifahrenheit(); /* Buat fungsi hitung konversi*/  
+}
+/* Fungsi hitung konversi */
+function hitungKonversicelcius() {
+    const suhu = parseFloat(incelcius.value);
+    let hasil;
+    hasil = suhu * 9/5 + 32; /* rumus konversi celcius ke fahrenheit */
+    
+    if (isNaN(hasil)) {
+        outfahrenheit.value = "eh... ha...apa itu"; /* pesan jika bukan angka */
+    } else {
+        outfahrenheit.value = hasil.toFixed(2);
+    }
+}
+function hitungKonversifahrenheit() {
+    const suhu = parseFloat(infahrenheit.value);
+    let hasil;
+    hasil = (suhu - 32) * 5/9; /* rumus konversi fahrenheit ke celcius */
+    
+    if (isNaN(hasil)) {
+        outcelcius.value = "eh... ha...apa itu"; /* pesan jika bukan angka */
+    } else {
+        outcelcius.value = hasil.toFixed(2);
+    }
+}
+/* Fungsi kalkulasi rumus */
+function kalkulasicelcius() {
+    const suhu = incelcius.value;
+    let calcText;
+    calcText = suhu +" * (9/5) + 32 = " + outfahrenheit.value;
+    if (outfahrenheit.value === "eh... ha...apa itu") {
+        kalcelcius.value = "oh, tidak ada kalkulasi";
+    } else {
+        kalcelcius.value = calcText;
+    }
+}
+function kalkulasifahrenheit() {
+    const suhu = infahrenheit.value;
+    let calcText;
+    calcText = "("+ suhu +" - 32) * (5/9) = " + outcelcius.value;
+    if (outcelcius.value === "eh... ha...apa itu") {
+        kalfahrenheit.value = "oh, tidak ada kalkulasi";
+    } else {
+        kalfahrenheit.value = calcText;
+    }
+}
+
+
+function clearAllInputs(event) {
+    var allInputs = document.querySelectorAll('input');
+    allInputs.forEach(singleInput => singleInput.value = '');
+ }
